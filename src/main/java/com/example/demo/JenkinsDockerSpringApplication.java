3221dfe892348d3cd65344d10d1cc3f97af2ca9e
Lipsa Mohanty <lipsam.jobhunt@gmail.com>
2022-03-03 14:20:32 +0530

    first commit

---
+++ src/main/java/com/example/demo/JenkinsDockerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDockerSpringApplication.class, args);
+	}
+
+}
